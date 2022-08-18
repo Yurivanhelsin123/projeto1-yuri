8ce0b97b21dd75d94c647817136cc7d7fe94909f
yuri <yurivanhelsin12@hotmail.com>
2022-08-17 21:12:52 -0300

    commit inicial

---
+++ AumentodeSalario.java
@@ -0,0 +1,53 @@
+package br.com.modelo.impulsotec;
+import java.util.Scanner;
+
+public class AumentodeSalario {
+public static void main(String[] args) {
+	
+	System.out.println("Qual o valor do seu salario? ");		
+	Scanner  d = new Scanner(System.in);
+	double salario = d.nextDouble();	
+	System.out.println("Se você teve promoção de cargo digite 1, "
+	+ "se teve aumento de salario por dissídio digite 2,se não teve aumento digite 3 ");
+	
+	int opcao = d.nextInt();
+	double salariofinal;
+	if(opcao ==1) {
+		double aumento = 0.3;
+		salariofinal = (salario * aumento)+ salario;		
+	}else if(opcao == 2) {
+		
+		salariofinal =salario + 100;
+	}else {		
+		
+		salariofinal = salario;			
+	}
+			
+	System.out.println("Seu salário este ano será de "   +  salariofinal);			
+
+		//instrução switch
+	
+
+   
+}
+
+
+}
+
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
