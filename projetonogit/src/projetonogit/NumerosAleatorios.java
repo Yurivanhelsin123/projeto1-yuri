38f32d2136ee48c048f69cfbee2fc4b89c0c3e29
Yurivanhelsin123 <yurivanhelsin12@hotmail.com>
2022-08-19 12:09:05 -0300

    adicionei uma ArrayList

--- projetonogit/src/projetonogit/NumerosAleatorios.java
+++ projetonogit/src/projetonogit/NumerosAleatorios.java
@@ -1,10 +1,35 @@
 package projetonogit;
 
+import java.util.ArrayList;
+
 public class NumerosAleatorios {
+	
+	
+	
 public static void main(String[] args) {
 	
+	ArrayList<Integer> valoresagregados = new ArrayList<Integer>();
 	
+	
+	valoresagregados.add(90);
+	valoresagregados.add(23);
+	valoresagregados.add(89);
+	valoresagregados.add(12);
+	valoresagregados.add(34);
+	valoresagregados.add(78);
+	valoresagregados.add(12);
+
 	
 	System.out.println("vamos treinar numeros aleatórios");
+	
+	System.out.println(valoresagregados);
+	
+	
+	
+	
+	
+	
+	
+	
 }
 }
